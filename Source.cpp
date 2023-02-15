97727275e5a8bd1e8226a5cc13944e1e6c3f7d3f
Костя Рождественский <kosta7497@gmail.com>
2023-02-15 13:27:45 +0300

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <queue>
+
+using namespace std;
+
+int tab = 0;
+int num_of_tab = 0;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
